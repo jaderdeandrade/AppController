fbb7ce9d3e12953e92cdb4741446a8697c95a264
jaderdeandrade <jaderdeandrade@gmail.com>
2019-08-01 16:27:32 -0300

    Iniciando o projeto

---
+++ src/main/java/com/jda/controller/AppControllerApplication.java
@@ -0,0 +1,13 @@
+package com.jda.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppControllerApplication.class, args);
+	}
+
+}
